3456b6a8de4b2e0432fbb345da2733e289e017ef
Subham Raj <rajshubham199@gmail.com>
2025-06-11 08:50:42 +0530

    rendered dynamic data in product explorer

--- product-wishlist/src/components/ProductCard.jsx
+++ product-wishlist/src/components/ProductCard.jsx
@@ -1,20 +1,25 @@
-const ProductCard = (data) => {
+const ProductCard = ({data}, handleClick) => {
+  // console.log(data)
+  let originalPrice = (data.price/(1-(data.discountPercentage/100))).toFixed(2)
+  console.log(handleClick)
+  // console.log(originalPrice)
+  // console.log(item.id , category , description, discountPercentage, price, rating, images[0])
   return (
     <>
       <div className="bg-white h-auto flex flex-col rounded shadow p-5 gap-2">
         <span className="px-2 rounded-full bg-red-500 text-white absolute ml-2 mt-2 w-fit text-sm">
-          -10%
+          {data.discountPercentage}%
         </span>
-        <img src="https://placehold.co/50x50" alt="" />
-        <span className="px-3 py-1 bg-gray-50 rounded-full w-fit text-sm">beauty</span>
+        <img src={data.images[0]} alt="" className="w-70 " />
+        <span className="px-3 py-1 bg-gray-50 rounded-full w-fit text-sm">{data.category}</span>
         <span className="text-lg font-semibold">
-          Essence Mascara Lash Princess
+          {data.title}
         </span>
-        <span className="text-xs text-gray-500">⭐ 2.6</span>
+        <span className="text-xs text-gray-500">⭐ {data.rating}</span>
         <span className="text-xl text-blue-700 mt-3 font-bold">
-          $8.94{" "}
+          ${data.price}{" "}
           <span className="text-sm text-gray-700 line-through font-semibold">
-            $9.99
+            ${originalPrice}
           </span>
         </span>
       </div>
